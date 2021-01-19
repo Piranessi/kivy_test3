4d16e03f6247c3339cf4f0df1aa53f3caf6d8d37
Piranessi <matgorzelak@gmail.com>
2021-01-20 00:49:20 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,9 +2,12 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from plyer import battery
+from plyer import vibrator
 
 class MainApp(App):
     def build(self):
+        time = 2
+        vibrator.vibrate(time=time)
         button = Button(text='Hello from Kivy',
                       size_hint=(.5, .5),
                       pos_hint={'center_x': .5, 'center_y': .5})
