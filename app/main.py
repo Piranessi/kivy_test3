07244340d928b0fdac5cfd9306389ca3c293068a
Piranessi <matgorzelak@gmail.com>
2021-01-20 00:38:35 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,18 +1,15 @@
 from jnius import autoclass
 from kivy.app import App
 from kivy.uix.button import Button
-# from plyer.notification import notify
+from plyer import battery
 
-PythonActivity = autoclass('org.renpy.android.PythonActivity')
-activity = PythonActivity.mActivity
-Context = autoclass('android.content.Context')
-vibr = activity.getSystemService(Context.VIBRATOR_SERVICE)
 
 
 
 class MainApp(App):
     def build(self):
-        vibr.vibrate(1000)
+
+
         button = Button(text='Hello from Kivy',
                       size_hint=(.5, .5),
                       pos_hint={'center_x': .5, 'center_y': .5})
