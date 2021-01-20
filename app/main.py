2f6a3ba184588a2c1b6e797c0c2943b95e79d59d
Piranessi <matgorzelak@gmail.com>
2021-01-20 20:36:37 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,16 +2,13 @@ from kivy.app import App
 from kivy.uix.button import Button
 from jnius import autoclass
 
-PythonActivity = autoclass('org.renpy.android.PythonActivity')
-activity = PythonActivity.mActivity
-Context = autoclass('android.content.Context')
-vibrator = activity.getSystemService(Context.VIBRATOR_SERVICE)
-
 
 class MainApp(App):
     def build(self):
-        vibrator.vibrate(10000)
-        button = Button(text='chuj z tym',
+        DisplayMetrics = autoclass('android.util.DisplayMetrics')
+        metrics = DisplayMetrics()
+
+        button = Button(text=str(metrics.getDeviceDensity()),
                       size_hint=(.5, .5),
                       pos_hint={'center_x': .5, 'center_y': .5})
 
