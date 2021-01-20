82e0b7c093a946975a2581f9abe19de2096a8d8b
Piranessi <matgorzelak@gmail.com>
2021-01-20 20:16:37 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
-# from jnius import autoclass
 from kivy.app import App
 from kivy.uix.button import Button
-from plyer import bluetooth
 from jnius import autoclass
-import android
 
 PythonActivity = autoclass('org.renpy.android.PythonActivity')
 activity = PythonActivity.mActivity
@@ -13,9 +10,8 @@ vibrator = activity.getSystemService(Context.VIBRATOR_SERVICE)
 
 class MainApp(App):
     def build(self):
-        str_bluetooth_info = str(bluetooth.info)
         vibrator.vibrate(10000)
-        button = Button(text=str_bluetooth_info,
+        button = Button(text='chuj z tym',
                       size_hint=(.5, .5),
                       pos_hint={'center_x': .5, 'center_y': .5})
 
