9316a9864cec91a180c623903fec9587955fb945
Piranessi <matgorzelak@gmail.com>
2021-01-21 13:58:53 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,28 +9,27 @@ BluetoothSocket = autoclass('android.bluetooth.BluetoothSocket')
 UUID = autoclass('java.util.UUID')
 
 
-
 def insert_newlines(string, every=32):
     lines = []
     for i in range(0, len(string), every):
         lines.append(string[i:i+every])
     return '\n'.join(lines)
 
-def get_socket_stream():
-    #paired_devices = BluetoothAdapter.getDefaultAdapter().getBondedDevices().toArray()
-    # str_var = 'wtf?'
-    # str_var = str(paired_devices)
-    #socket = None
-    # for device in paired_devices:
-    #     if device.getName() == name:
-    #         socket = device.createRfcommSocketToServiceRecord(
-    #             UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"))
-    #         recv_stream = socket.getInputStream()
-    #         send_stream = socket.getOutputStream()
-    #         break
-    # socket.connect()
-    # return recv_stream, send_stream
-    pass
+# def get_socket_stream():
+#     #paired_devices = BluetoothAdapter.getDefaultAdapter().getBondedDevices().toArray()
+#     # str_var = 'wtf?'
+#     # str_var = str(paired_devices)
+#     #socket = None
+#     # for device in paired_devices:
+#     #     if device.getName() == name:
+#     #         socket = device.createRfcommSocketToServiceRecord(
+#     #             UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"))
+#     #         recv_stream = socket.getInputStream()
+#     #         send_stream = socket.getOutputStream()
+#     #         break
+#     # socket.connect()
+#     # return recv_stream, send_stream
+#     pass
 
 class MainApp(App):
     def build(self):
