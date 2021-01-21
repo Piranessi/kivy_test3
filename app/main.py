007d753a7c77264b1e07e124724b4962e5b69265
Piranessi <matgorzelak@gmail.com>
2021-01-21 14:08:54 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -40,7 +40,7 @@ class MainApp(App):
         except Exception as e:
             str_var = traceback.format_exc()
 
-        button = Button(text=str(insert_newlines(str_var)),
+        button = Button(text="test",
                       size_hint=(.5, .5),
                       pos_hint={'center_x': .5, 'center_y': .5})
 
