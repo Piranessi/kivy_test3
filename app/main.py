7c4b00122f0754c2c1685afe72eb5f4d8fe14efb
Piranessi <matgorzelak@gmail.com>
2021-01-21 13:06:02 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from kivy.app import App
 from kivy.uix.button import Button
 from jnius import autoclass
 
+where_err = 0
+
 def insert_newlines(string, every=32):
     lines = []
     for i in range(0, len(string), every):
@@ -16,18 +18,24 @@ class MainApp(App):
             bt_adapter = bt.getDefaultAdapter()
 
             # This one works with SDL2
+            where_err = 1
             PythonActivity = autoclass('org.kivy.android.PythonActivity')
 
+            where_err = 2
             activity = PythonActivity.mActivity
 
+            where_err = 3
             Context = autoclass('android.content.Context')
+
+            where_err = 4
             vibrator = activity.getSystemService(Context.VIBRATOR_SERVICE)
 
+            where_err = 5
             vibrator.vibrate(10000)  # the argument is in milliseconds
 
 
         except Exception as e:
-            str_var = str(e)
+            str_var = str(str(where_err)+e)
 
 
 
