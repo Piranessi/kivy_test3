a94e350a8533bf3cc7e3d89935b4dfcf0b794309
Piranessi <matgorzelak@gmail.com>
2021-01-21 14:33:19 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -17,28 +17,28 @@ def insert_newlines(string, every=32):
     else:
         return string
 
-# def get_socket_stream():
-#     #paired_devices = BluetoothAdapter.getDefaultAdapter().getBondedDevices().toArray()
-#     # str_var = 'wtf?'
-#     # str_var = str(paired_devices)
-#     #socket = None
-#     # for device in paired_devices:
-#     #     if device.getName() == name:
-#     #         socket = device.createRfcommSocketToServiceRecord(
-#     #             UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"))
-#     #         recv_stream = socket.getInputStream()
-#     #         send_stream = socket.getOutputStream()
-#     #         break
-#     # socket.connect()
-#     # return recv_stream, send_stream
-#     pass
+def get_socket_stream():
+    paired_devices = BluetoothAdapter.getDefaultAdapter().getBondedDevices().toArray()
+    # str_var = 'wtf?'
+    # str_var = str(paired_devices)
+    #socket = None
+    # for device in paired_devices:
+    #     if device.getName() == name:
+    #         socket = device.createRfcommSocketToServiceRecord(
+    #             UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"))
+    #         recv_stream = socket.getInputStream()
+    #         send_stream = socket.getOutputStream()
+    #         break
+    # socket.connect()
+    # return recv_stream, send_stream
+    pass
 
 class MainApp(App):
     def build(self):
         str_var = "all g"
 
         try:
-            a = 5/0
+            get_socket_stream()
         except Exception as e:
             str_var = traceback.format_exc()
 
