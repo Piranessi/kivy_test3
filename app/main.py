8f58c1b5562b40509f723d9c2f6ccba1f8550b4f
Piranessi <matgorzelak@gmail.com>
2021-01-21 14:23:27 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -44,7 +44,7 @@ class MainApp(App):
             str_var = traceback.format_exc()
 
         # str_var = insert_newlines(str_var)
-        button = Button(text=str_var,
+        button = Button(text=str(str_var),
                       size_hint=(.5, .5),
                       pos_hint={'center_x': .5, 'center_y': .5})
 
