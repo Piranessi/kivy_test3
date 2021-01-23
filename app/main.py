36268bb5327a653ffd225bb1a8756e2d94a356fb
Piranessi <matgorzelak@gmail.com>
2021-01-23 16:43:36 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -32,9 +32,6 @@ class MainApp(App):
             self.debug_str = traceback.format_exc()
 
         self.debug_str = insert_newlines(self.debug_str)
-
-
-        # on_release=UrlRequest(url="https://www.olx.pl"),
         button = Button(text=self.debug_str,
                         on_release=UrlRequest(url="https://www.olx.pl"),
                         size_hint=(.5, .5),
