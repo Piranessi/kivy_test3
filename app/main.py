780c3a35a0965ab382f632cdfd84ab1f00a830f0
Piranessi <matgorzelak@gmail.com>
2021-01-23 15:55:19 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -17,8 +17,13 @@ def insert_newlines(string, every=32):
 
 class MainApp(App):
     def build(self):
-        soup = BeautifulSoup(urllib2.urlopen("https://www.olx.pl"))
-        str_var = soup.title.string
+        str_var = 'init'
+        try:
+            soup = BeautifulSoup(urllib2.urlopen("https://www.olx.pl"))
+            str_var = soup.title.string
+        except Exception as e:
+            str_var = traceback.format_exc()
+
         str_var = insert_newlines(str_var)
         button = Button(text=str_var,
                       size_hint=(.5, .5),
