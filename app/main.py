385e1a1c66c208fc32f473e387d4965d182b9229
Piranessi <matgorzelak@gmail.com>
2021-01-24 17:26:12 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -27,7 +27,8 @@ class MainApp(App):
         self.result = UrlRequest(url=url_str,
                                  on_success=print('Instalacja urzadzenia zakonczona.'),
                                  req_body='test-req_body',
-                                 on_error='Sprawdz polaczenie wifi z urzadzeniem.')
+                                 on_error='Sprawdz polaczenie wifi z urzadzeniem.',
+                                 req_header='test')
 
     def build(self):
         # try:
