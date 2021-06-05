3adf0f8ec369b445a800f3e77a16d7a8052461d8
Mateusz <matgorzelak@gmail.com>
2021-06-05 13:44:56 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,33 @@ from kivy.network.urlrequest import UrlRequest
 from kivy.app import App
 from kivy.uix.button import Button
 import socket
+import json
+import struct
+
+########### related to sending/receiving over socket
+#BLOB = Binary Large OBject
+#msg has to be serialized JSON
+def read_blob(sock, size):
+    buf = b''
+    while len(buf) != size:
+        ret = sock.recv(size - len(buf))
+        if not ret:
+            raise ConnectionError("Socket closed")
+        buf += ret
+    return buf
+
+
+def read_long(sock):
+    size = struct.calcsize("L")
+    data = read_blob(sock, size)
+    return struct.unpack("L", data)[0] #struct.unpack() returns tuple even one element inside
+
+
+def read_from_socket(sock):
+    buffer_size = read_long(sock)
+    data = read_blob(sock, buffer_size)
+    return json.loads(data)
+####################################################
 
 
 def insert_newlines(string, every=32):
@@ -19,27 +46,30 @@ class MainApp(App):
     debug_str = 'init'
     button = None
     result = None
-    wifi_name = 'krul_m'
-    wifi_password = 'BDJQp!#'
 
     def button_on_error(self,*args):
         self.button.text = 'connection error'
 
     def setup_device(self, *args):
         self.button.text = 'setup: init'
-        msg = self.wifi_name + self.wifi_password + "#" + str(len(self.wifi_name)) + "#" + str(len(self.wifi_password))
-        encoded_msg = bytes(msg, "utf-8")
-        s = socket.socket()
+
+
+        cfg_dict = {
+            'wifi_ssid': 'Zyku',
+            'wifi_password': 'jarecki1!'
+        }
+        cfg_dict_json = json.dumps(cfg_dict)
+        print('len:', len(cfg_dict_json))
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         ip = '192.168.4.1'
         port = 3000
-        print(msg)
+
         try:
-            s.connect((ip,port))
-            s.send(encoded_msg)
-            # data = s.recv(1024)
-            # decoded_data = data.decode("utf-8")
-            # print(decoded_data + " to odebralem")
-            s.close()
+            sock.connect((ip,port))
+            sock.sendall(struct.pack("L", len(cfg_dict_json)))
+            sock.sendall(bytes(cfg_dict_json,encoding='utf-8'))
+            sock.close()
             self.button.text = 'msg sent, connection closed'
         except Exception as e:
             self.button.text = insert_newlines(traceback.format_exc())
